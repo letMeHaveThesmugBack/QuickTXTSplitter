e51bfa10ad1f40ef4be60b26bc402533c3232cfc
thesmug <148411048+letMeHaveThesmugBack@users.noreply.github.com>
2025-08-29 23:53:50 -0400

    Simplify ArgParser.Parse and implement error handling on ArgParser and FileSplitter

--- ArgParser.cs
+++ ArgParser.cs
@@ -1,9 +1,12 @@
 ﻿using System.Text.RegularExpressions;
+using static QuickTXTSplitter.ErrorHandler;
 
 namespace QuickTXTSplitter
 {
     internal static class ArgParser
     {
+        const string operation = "parse command-line arguments";
+
         internal enum ParseState
         {
             None,
@@ -18,71 +21,75 @@ namespace QuickTXTSplitter
 
         internal static ParsedArgs Parse(string[] args)
         {
-            string currentArg;
-
-            ParseState state = ParseState.None;
-
-            DirectoryInfo? source = null;
-            DirectoryInfo? destination = null;
-            Regex? regex = null;
-            int capturingGroup = 0;
-            string prefix = string.Empty;
-
-            for (int i = 0; i < args.Length; i++)
+            try
             {
-                currentArg = args[i];
+                ParseState state = ParseState.None;
 
-                if (currentArg.StartsWith('-'))
+                DirectoryInfo? source = null;
+                DirectoryInfo? destination = null;
+                Regex? regex = null;
+                int capturingGroup = 0;
+                string prefix = string.Empty;
+
+                foreach (string arg in args)
                 {
-                    string option = args[i].ToLower()[1..];
-
-                    state = option switch
+                    if (arg.StartsWith('-'))
                     {
-                        "-source" or "s" => ParseState.Source,
-                        "-dest" or "d" => ParseState.Destination,
-                        "-regex" or "r" => ParseState.Regex,
-                        "-capturingGroup" or "c" => ParseState.CapturingGroup,
-                        "-prefix" or "p" => ParseState.Prefix,
-                        _ => ParseState.None,
-                    };
+                        string option = arg.ToLower()[1..];
+
+                        state = option switch
+                        {
+                            "-source" or "s" => ParseState.Source,
+                            "-dest" or "d" => ParseState.Destination,
+                            "-regex" or "r" => ParseState.Regex,
+                            "-capturingGroup" or "c" => ParseState.CapturingGroup,
+                            "-prefix" or "p" => ParseState.Prefix,
+                            _ => ParseState.None,
+                        };
+                    }
+
+                    else
+                    {
+                        InterpretOption(arg);
+
+                        state = ParseState.None;
+                    }
                 }
 
-                else
+                void InterpretOption(string arg)
                 {
-                    InterpretOption();
-
-                    state = ParseState.None;
+                    switch (state)
+                    {
+                        case ParseState.Source:
+                            source = new(Path.GetFullPath(arg));
+                            break;
+                        case ParseState.Destination:
+                            destination = new(Path.GetFullPath(arg));
+                            break;
+                        case ParseState.Regex:
+                            regex = new Regex(arg);
+                            break;
+                        case ParseState.CapturingGroup:
+                            if (!int.TryParse(arg, out capturingGroup)) capturingGroup = 0; // TryParse normally sets out to 0 if it fails, but this just ensures it definitely gives 0 on failure in case anything strange happens.
+                            break;
+                        case ParseState.Prefix:
+                            prefix = arg;
+                            break;
+                        default:
+                            throw new ArgumentException($"""Unexpected argument "{arg}".""");
+                    }
                 }
+
+                return regex is null
+                    ? throw new ArgumentException("""The "--regex (-r)" argument is required.""")
+                    : new(source ?? new(Directory.GetCurrentDirectory()), destination ?? new(Directory.GetCurrentDirectory()), regex, capturingGroup, prefix);
             }
 
-            void InterpretOption()
+            catch (Exception ex)
             {
-                switch (state)
-                {
-                    case ParseState.Source:
-                        source = new(Path.GetFullPath(currentArg));
-                        break;
-                    case ParseState.Destination:
-                        destination = new(Path.GetFullPath(currentArg));
-                        break;
-                    case ParseState.Regex:
-                        regex = new Regex(currentArg);
-                        break;
-                    case ParseState.CapturingGroup:
-                        if (!int.TryParse(currentArg, out capturingGroup)) capturingGroup = 0; // TryParse normally sets out to 0 if it fails, but this just ensures it definitely gives 0 on failure in case anything strange happens.
-                        break;
-                    case ParseState.Prefix:
-                        prefix = currentArg;
-                        break;
-                    default:
-                        // do something
-                        break;
-                }
+                WriteStandardizedErrorMessageAndExit(operation, ex);
+                throw; // This exists to satisfy the compiler that complains that not all paths return a value, and cannot ever be reached because the above function exits the program.
             }
-
-            if (regex is null) throw new NotImplementedException("say something about requiring the regex");
-
-            return new(source ?? new(Directory.GetCurrentDirectory()), destination ?? new(Directory.GetCurrentDirectory()), regex, capturingGroup, prefix);
         }
     }
 }
--- FileSplitter.cs
+++ FileSplitter.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text;
 using System.Text.RegularExpressions;
 using static QuickTXTSplitter.DirectoryBuilder;
+using static QuickTXTSplitter.ErrorHandler;
 using ConcurrentCollections;
 using System.Collections.Immutable;
 
@@ -8,6 +9,8 @@ namespace QuickTXTSplitter
 {
     internal static partial class FileSplitter
     {
+        static readonly StringBuilder operationBuilder = new("initialize the file splitter");
+
         static readonly ConcurrentHashSet<string> nonAlphanumericFilenames = new(4, 1_500_000);
         static readonly ImmutableDictionary<char, ConcurrentHashSet<string>> alphanumericFilenames = new Dictionary<char, ConcurrentHashSet<string>>
         {
@@ -51,91 +54,101 @@ namespace QuickTXTSplitter
 
         internal static void Split(string sourceFilepath, ArgParser.ParsedArgs args)
         {
-            DirectoryInfo destinationInfo = args.Destination;
-            Regex regex = args.Regex;
-            int capturingGroup = args.CapturingGroup;
-            string prefix = args.Prefix;
-
-            IEnumerable<string> sourceLines = File.ReadLines(sourceFilepath);
-
-            StreamWriter? splitFileWriter = null;
-            string splitFilepath = string.Empty;
-
-            foreach (string line in File.ReadLines(sourceFilepath))
+            try
             {
-                if (splitFileWriter is null)
+                DirectoryInfo destinationInfo = args.Destination;
+                Regex regex = args.Regex;
+                int capturingGroup = args.CapturingGroup;
+                string prefix = args.Prefix;
+
+                IEnumerable<string> sourceLines = File.ReadLines(sourceFilepath);
+
+                StreamWriter? splitFileWriter = null;
+                string splitFilepath = string.Empty;
+
+                foreach (string line in File.ReadLines(sourceFilepath))
                 {
-                    if (!TrySplitAtLine(line, out splitFilepath)) splitFilepath = Path.Combine(destinationInfo.FullName, NoMatchSubdirectory, $"{prefix}{NoMatchSubdirectory}{Guid.NewGuid()}.txt");
-                    splitFileWriter = new(splitFilepath);
+                    operationBuilder.Clear().Append("split ").Append(sourceFilepath).Append("at line \"").Append(line).Append('"');
+
+                    if (splitFileWriter is null)
+                    {
+                        if (!TrySplitAtLine(line, out splitFilepath)) splitFilepath = Path.Combine(destinationInfo.FullName, NoMatchSubdirectory, $"{prefix}{NoMatchSubdirectory}{Guid.NewGuid()}.txt");
+                        splitFileWriter = new(splitFilepath);
+                    }
+
+                    else if (TrySplitAtLine(line, out splitFilepath))
+                    {
+                        splitFileWriter?.Dispose();
+
+                        splitFileWriter = new(splitFilepath);
+                    }
+
+                    splitFileWriter.WriteLine(line);
                 }
 
-                else if (TrySplitAtLine(line, out splitFilepath))
+                splitFileWriter?.Dispose();
+
+                bool TrySplitAtLine(string line, out string filepath)
                 {
-                    splitFileWriter?.Dispose();
+                    string title;
 
-                    splitFileWriter = new(splitFilepath);
-                }
+                    Match match = regex.Match(line);
 
-                splitFileWriter.WriteLine(line);
-            }
+                    if (match.Success && match.Groups.Count >= capturingGroup + 1)
+                    {
+                        title = match.Groups[capturingGroup].Value.TrimStart();
+                        string titleLower = title.ToLower();
 
-            splitFileWriter?.Dispose();
-
-            bool TrySplitAtLine(string line, out string filepath)
-            {
-                string title;
-
-                Match match = regex.Match(line);
-
-                if (match.Success && match.Groups.Count >= capturingGroup + 1)
-                {
-                    title = match.Groups[capturingGroup].Value.TrimStart();
-                    string titleLower = title.ToLower();
-
-                    List<char> chars = [.. from rune in titleLower.EnumerateRunes()
+                        List<char> chars = [.. from rune in titleLower.EnumerateRunes()
                                            select TextUtilities.NormalizeToSubdirectoryCharacter(rune)];
 
-                    string subdirectory = chars.Count switch
+                        string subdirectory = chars.Count switch
+                        {
+                            0 => Path.Combine("$", "$$"),
+                            1 => !chars[0].IsForeign() ? Path.Combine($"{chars[0]}", $"{chars[0]}{chars[0]}") : ForeignSubdirectory,
+                            _ => !chars[0].IsForeign() && !chars[1].IsForeign()
+                                                            ? Path.Combine($"{chars[0]}", $"{chars[0]}{chars[1]}")
+                                                            : ForeignSubdirectory,
+                        };
+
+                        title = title.Sanitize();
+                        StringBuilder filenameBuilder = new($"{prefix}{title}.txt");
+
+                        ConcurrentHashSet<string> existingFilenameSet = subdirectory[0] switch
+                        {
+                            '$' or '[' => nonAlphanumericFilenames,
+                            _ => alphanumericFilenames[subdirectory[0]],
+                        };
+
+                        while (!existingFilenameSet.Add(filenameBuilder.ToString()))
+                        {
+                            Regex regex = IncrementalFilenameRegex();
+                            Match filenameMatch = regex.Match(filenameBuilder.ToString());
+                            int index = filenameMatch.Success ? int.Parse(filenameMatch.Groups[2].Value) + 1 : 1;
+
+                            filenameBuilder.Clear();
+
+                            filenameBuilder.Append(filenameMatch.Groups[1].Value)
+                                .Append("[#")
+                                .Append(index)
+                                .Append("].txt");
+                        } // todo: this does not work. it just does empty then [#...]. figure ot why the first capturing group is being lost
+
+                        filepath = Path.Combine(destinationInfo.FullName, subdirectory, filenameBuilder.ToString());
+                        return true;
+                    }
+
+                    else
                     {
-                        0 => Path.Combine("$", "$$"),
-                        1 => !chars[0].IsForeign() ? Path.Combine($"{chars[0]}", $"{chars[0]}{chars[0]}") : ForeignSubdirectory,
-                        _ => !chars[0].IsForeign() && !chars[1].IsForeign()
-                                                        ? Path.Combine($"{chars[0]}", $"{chars[0]}{chars[1]}")
-                                                        : ForeignSubdirectory,
-                    };
-
-                    title = title.Sanitize();
-                    StringBuilder filenameBuilder = new($"{prefix}{title}.txt");
-
-                    ConcurrentHashSet<string> existingFilenameSet = subdirectory[0] switch
-                    {
-                        '$' or '[' => nonAlphanumericFilenames,
-                        _ => alphanumericFilenames[subdirectory[0]],
-                    };
-
-                    while (!existingFilenameSet.Add(filenameBuilder.ToString()))
-                    {
-                        Regex regex = IncrementalFilenameRegex();
-                        Match filenameMatch = regex.Match(filenameBuilder.ToString());
-                        int index = filenameMatch.Success ? int.Parse(filenameMatch.Groups[2].Value) + 1 : 1;
-
-                        filenameBuilder.Clear();
-
-                        filenameBuilder.Append(filenameMatch.Groups[1].Value)
-                            .Append("[#")
-                            .Append(index)
-                            .Append("].txt");
-                    } // todo: this does not work. it just does empty then [#...]. figure ot why the first capturing group is being lost
-
-                    filepath = Path.Combine(destinationInfo.FullName, subdirectory, filenameBuilder.ToString());
-                    return true;
+                        filepath = string.Empty;
+                        return false;
+                    }
                 }
+            }
 
-                else
-                {
-                    filepath = string.Empty;
-                    return false;
-                }
+            catch (ArgumentException ex)
+            {
+                WriteStandardizedErrorMessageAndExit(operationBuilder.ToString(), ex);
             }
         }
 
